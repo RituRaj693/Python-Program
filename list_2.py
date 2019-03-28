72e4f0eae8dcf3b1557277341333e5ff687d25f5
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-03-28 19:31:15 +0530

    Add files via upload

---
+++ list_2.py
@@ -0,0 +1,25 @@
+############## add items in the list or append() method
+##thislist = ['ram','shyam','bhanu']
+#thislist.append('raj')
+#print(thislist)
+
+
+
+###########to add the items at the specified index by insert() method
+#thislist = ['ram','shyam','bhanu']
+#thislist.insert(2,'jnkjh',)
+#thislist.remove('ram')
+
+#print(thislist)
+
+############# pop() method to remove the specified index or the lst item of the list
+#thislist = ['ram','shyam','bhanu']
+#thislist.pop()
+#print(thislist)
+
+
+
+################################### the list constructor
+thislist = list(('ram','shyam','bhau'))
+print((thislist))
+
