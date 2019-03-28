2834e1d8d12cf26ededcab1ab6a8727df2b9ea17
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-03-28 19:04:22 +0530

    Add files via upload

---
+++ list_1.py
@@ -0,0 +1,33 @@
+#thislist = ['ram','shyam','ramu']
+#print(thislist[1])
+##################### you acess the list number by index number
+
+
+
+#########change the value in list
+#thislist = ['ram','shyam','bhanu']
+#thislist[0] = 'apple'
+#thislist[1] = 'tata'
+#print(thislist)
+
+##########loop through the list
+#thislist = ['ram','shyam','bhanu']
+#for x in thislist:
+   # print(x)
+
+
+   ############ check the item is exist or not
+#thislist =['ram','shyam','bhanu']
+#if 'ram' in thislist:
+   # print('ram present in the list')
+#print(thislist)
+
+
+###############to find the length of list
+#thislist = ['ram','shyam','bhanu',1,2,3]
+#print(len(thislist))
+
+
+
+
+
