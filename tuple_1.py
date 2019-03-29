a84748ad23bd2b000d24d69e599a37321283500d
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-03-29 10:18:59 +0530

    Add files via upload

---
+++ tuple_1.py
@@ -0,0 +1,26 @@
+#thistuple = ('kohli','dhoni','raina')
+#print(thistuple[1])
+
+
+
+
+########change value in tuples
+#thistuple = ('kohli','dhoni','raina')
+#thistuple[1] = 'jhjhjkh'
+#print(thistuple)
+
+
+##############################for loop in tuple
+#thistuple = ('dhoni','raina','kohli')
+#for x in thistuple:
+  #  print(x)
+
+
+########check th eitem in the tuple
+#thistuple = ('dhoni','kholi','raina')
+#if 'dhoni' in thistuple:
+  #  print("dh")
+
+  ######### length of tuple
+thistuple = ('dhoni','raina','watson')
+print(len(thistuple))
