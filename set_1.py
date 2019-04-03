54b60c46673a8970d360b8ec2c0e64ac2096b9b7
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-04-03 09:36:37 +0530

    Add files via upload

---
+++ set_1.py
@@ -0,0 +1,20 @@
+#thisset = {'kohli','dhoni','raina'}
+#print(thisset)
+
+########loop through set
+#thisset = {'kohli','dhoni','raina'}
+#for x in thisset:
+ #   print(x)
+
+
+
+ ##################### check the items in set
+#thisset = {'dhoni','kohli','raina'}
+#print('dhoni' in thisset)
+
+
+
+##################add items in set
+#thisset = {'dhoni','raina','kohli'}
+#thisset.add('yuvraj')
+#print(thisset)
