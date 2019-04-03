aad8a5c9093685df8901d50ff359fc820f300779
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-04-03 09:50:55 +0530

    Add files via upload

---
+++ set_2.py
@@ -0,0 +1,26 @@
+##############################update the set
+#thisset = {'kohli','raina'}
+#thisset.update(['dhoni','tendulkar'])
+#print(thisset)
+
+######get the lenght of set
+#thisset = ('raina','dhoni','yuvraj')
+#print(len(thisset))
+
+
+######################### remove item from set
+#thisset = {'dhoni','raina','jhdav'}
+#thisset.remove('jhdav')
+#print(thisset)
+
+
+####################################remove item  from discard method
+#thisset = ('kohli','raina','dhoni')
+#thisset.discard('raina')
+#print(thisset)
+
+
+######################  pop method in set
+thisset = {'raian','dhoni','raina','yuvraj'}
+thisset.pop()
+print(thisset)
